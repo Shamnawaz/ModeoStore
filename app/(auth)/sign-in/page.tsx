c293ead5b7515012d4f00d182eb89dc3c8d45082
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-06-18 15:41:59 +0200

    Auth Layout + Sign In Page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,30 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { APP_NAME } from "@/lib/constants";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+    title: 'Login',
+}
+
+const SignInPage = () => {
+    return ( 
+        <div className="w-full max-w-md mx-auto">
+            <Card className="py-10">
+                <CardHeader className="space-y-4">
+                    <Link href={'/'} className="flex-center">
+                        <Image src={'/images/logo.svg'} width={100} height={100} alt={`${APP_NAME} logo`} priority={true}/>
+                    </Link>
+                    <CardTitle className="text-center">Se Connecter</CardTitle>
+                    <CardDescription className="text-center">Connectez-vous à votre compte</CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                    {/* Form */}
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
+ 
+export default SignInPage;
