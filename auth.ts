49bc087380b198d84a85613a0534529c9c92ba37
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-06-18 17:28:57 +0200

    Custom JWT Callback

--- auth.ts
+++ auth.ts
@@ -51,6 +51,8 @@ export const config = {
 
             // Set the user ID from the token
             session.user.id = token.sub;
+            session.user.role = token.role;
+            session.user.name = token.name
 
             // If there's an update, set the user name
             if(trigger === 'update') {
@@ -59,6 +61,22 @@ export const config = {
 
             return session
         },
+
+        async jwt({ token, user, trigger, session }: any) {
+            if(user) {
+                token.role = user.role;
+
+                if(user.name === 'NO_NAME') {
+                    token.name = user.email!.split('@')[0];
+
+                    await prisma.user.update({
+                        where: { id: user.id },
+                        data: { name: token.name }
+                    })
+                }
+            }
+            return token;
+        }
     }
 } satisfies NextAuthConfig;
 
