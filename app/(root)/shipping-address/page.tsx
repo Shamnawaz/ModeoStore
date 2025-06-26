85c88df931aa5d32351698c16ed19450134be61e
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-06-26 10:35:35 +0200

    Checkout Steps Component

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
 import { getUserById } from "@/lib/actions/user.actions";
 import ShippingAddressForm from "./shipping-address-form";
+import CheckoutSteps from "@/components/shared/checkout-steps";
 
 export const metadata: Metadata = {
     title: 'Adresse de livraison',
@@ -23,6 +24,7 @@ const ShippingAdressPage = async () => {
 
     return ( 
         <>
+            <CheckoutSteps current={1} />
             <ShippingAddressForm address={user.address as ShippingAddress} />
         </>
     );
