0bb36c80242acc9e3041dfc6afb00d291199707d
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-07-14 17:16:41 +0200

    Update Profile Form

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,8 +1,23 @@
-const ProfilePage = () => {
+import { Metadata } from "next";
+import { auth } from "@/auth";
+import { SessionProvider } from 'next-auth/react';
+import ProfileForm from "./profile-form";
+
+export const metadata: Metadata = {
+    title: 'Profil'
+}
+
+const ProfilePage = async () => {
+
+    const session = await auth();
+
     return ( 
-        <>
-            Profile Page
-        </>
+        <SessionProvider session={session}>
+            <div className="max-w-md mx-auto space-y-4">
+                <h2 className="h2-bold">Profil</h2>
+                <ProfileForm />
+            </div>
+        </SessionProvider>
     );
 }
  
